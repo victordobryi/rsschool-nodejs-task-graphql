25cc813e16d13dec3dcf18a52736f5070bda29dc
bazla <dobr1nya@mail.ru>
2024-03-05 02:06:16 +0300

    feat: add loader-prime

--- src/routes/graphql/queries.ts
+++ src/routes/graphql/queries.ts
@@ -5,6 +5,11 @@ import { GraphQLContext } from './types/app.js';
 import { UUIDType } from './types/uuid.js';
 import { PostType } from './types/post.js';
 import { UserType } from './types/user.js';
+import {
+  ResolveTree,
+  parseResolveInfo,
+  simplifyParsedResolveInfoFragmentWithType,
+} from 'graphql-parse-resolve-info';
 
 export const query = new GraphQLObjectType({
   name: 'Query',
@@ -62,8 +67,42 @@ export const query = new GraphQLObjectType({
     },
     users: {
       type: new GraphQLList(UserType),
-      resolve: async (_source, _args, context: GraphQLContext) => {
-        return context.prisma.user.findMany();
+      resolve: async (_source, _args, context: GraphQLContext, info) => {
+        const parsedInfo = parseResolveInfo(info) as ResolveTree;
+        const { fields } = simplifyParsedResolveInfoFragmentWithType(
+          parsedInfo,
+          UserType,
+        );
+
+        const includeSubscribedToUser = Boolean(fields['subscribedToUser']);
+        const includeUserSubscribedTo = Boolean(fields['userSubscribedTo']);
+
+        const users = await context.prisma.user.findMany({
+          include: {
+            subscribedToUser: includeSubscribedToUser,
+            userSubscribedTo: includeUserSubscribedTo,
+          },
+        });
+
+        if (includeSubscribedToUser) {
+          users.forEach((user) => {
+            const subscribers = users.filter((u) =>
+              u.subscribedToUser.some((sub) => sub.subscriberId === user.id),
+            );
+            context.loaders.subscribedToUser.prime(user.id, subscribers);
+          });
+        }
+
+        if (includeUserSubscribedTo) {
+          users.forEach((user) => {
+            const authors = users.filter((u) =>
+              u.userSubscribedTo.some((sub) => sub.authorId === user.id),
+            );
+            context.loaders.userSubscribedTo.prime(user.id, authors);
+          });
+        }
+
+        return users;
       },
     },
     user: {
